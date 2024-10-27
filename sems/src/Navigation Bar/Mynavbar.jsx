e97993f74c1acdac7e86ce6a5592c0d8f9e55aa4
Shalzzz03 <shalinchore@gmail.com>
2024-10-27 11:04:17 +0530

    Adding Links to navbar

--- sems/src/Navigation Bar/Mynavbar.jsx
+++ sems/src/Navigation Bar/Mynavbar.jsx
@@ -9,60 +9,23 @@ const Mynavbar = () => {
             <div className={styles.hOMEText}>
               <div className={styles.eLLIPSE} />
             </div>
-            <div className={styles.home}>HOME</div>
+            <a href="/" className={styles.home}>HOME</a>
             <div className={styles.cONTACTUSText}>
-              <div className={styles.sports}>SPORTS</div>
+              <a href="/sports" className={styles.sports}>SPORTS</a>
             </div>
             <div className={styles.aBOUTUSFrame}>
-              <div className={styles.equipments}>EQUIPMENTS</div>
+              <a href="/equipments" className={styles.equipments}>EQUIPMENTS</a>
             </div>
             <div className={styles.aBOUTUSFrame1}>
-              <div className={styles.aboutUs}>ABOUT US</div>
+              <a href="/aboutus" className={styles.aboutUs}>ABOUT US</a>
             </div>
-            <div className={styles.contactUs}>CONTACT US</div>
+            <a href="/contactus" className={styles.contactUs}>CONTACT US</a>
           </div>
           <div className={styles.div} />
           <div className={styles.fRAMEBChild} />
         </div>
       </header>
-      {/* <div className={styles.fORSTUDENTSFrame}>
-        <div className={styles.rectangleA} />
-        <div className={styles.fRAMEE}>
-          <div className={styles.fORFACULTYText}>
-            <div className={styles.aDMINLOGINFrame}>
-              <h2 className={styles.forFaculty}>FOR FACULTY</h2>
-              <div className={styles.userLoginText}>
-                <div className={styles.fRAMEF}>
-                  <div className={styles.adminLoginFrame} />
-                  <div className={styles.adminLogin}>ADMIN LOGIN</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.forFacultyFrame}>
-            <img
-              className={styles.groupIcon}
-              loading="lazy"
-              alt=""
-              src="/group.svg"
-            />
-          </div>
-          {/* <div className={styles.forStudentsFrame}>
-            <h2 className={styles.forStudents}>FOR STUDENTS</h2>
-            <div className={styles.forStudentsFrameChild} />
-            <div className={styles.toKnowEquipments}>
-              <i className={styles.toKnowAbout}>
-                To know about available equipments
-              </i>
-              <div className={styles.userFrame}>
-                <button className={styles.userLoginWrapper}>
-                  <div className={styles.userLogin}>USER LOGIN</div>
-                </button>
-              </div>
-            </div>
-          </div> */}
-        {/* </div> */}
-      {/* // </div>  */}
+     
     </div>
   );
 };
